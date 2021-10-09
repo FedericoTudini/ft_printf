d0a67084bc45a2ea6c77b36bafe4893ef3917cbd
FedericoTudini <fede.tudini98@gmail.com>
2021-10-09 18:50:47 +0200

    new version

--- Makefile
+++ Makefile
@@ -1,38 +1,65 @@
+#Variables
 
-NAME	=	libftprintf.a
+NAME		= libftprintf.a
+INCLUDES	= includes
+LIBFT		= libft
+SRCS_DIR	= src/
+OBJS_DIR	= bin/
+CC			= gcc
+CFLAGS		= -Wall -Werror -Wextra
+RM			= rm -f
+AR			= ar rcs
 
-CC	=	gcc
-CFLAGS	=	-Wall -Wextra -Werror
+# Colors
 
-RM	=	rm -f
+DEF_COLOR = \033[0;39m
+GRAY = \033[0;90m
+RED = \033[0;91m
+GREEN = \033[0;92m
+YELLOW = \033[0;93m
+BLUE = \033[0;94m
+MAGENTA = \033[0;95m
+CYAN = \033[0;96m
+WHITE = \033[0;97m
 
-AR	=	ar
-ARFLAGS	=	rcs
+#Sources
 
-SRCS	=		./srcs/ft_calloc.c \
-				./srcs/ft_convert.c \
-				./srcs/ft_itoa.c \
-				./srcs/ft_printf.c \
-				./srcs/ft_putchar_fd.c \
-				./srcs/ft_putnbr_base.c \
-				./srcs/ft_putnbr_fd.c \
-				./srcs/ft_putstr_fd.c \
-				./srcs/ft_strlen.c \
+SRC_FILES	= ft_printf.c ft_printf_utils.c ft_print_ptr.c ft_print_unsigned.c ft_print_hex.c
 
-OBJS = ${SRCS:.c=.o}
+#Binary
 
-.c.o: $(SRCS)
-	$(CC) $(CFLAGS) -c -o $@ $<
+SRCS 		= $(addprefix $(SRCS_DIR), $(SRC_FILES))
+OBJS 		= $(SRC_FILES:.c=.o) 
+OBJS_PREFIX	= $(addprefix $(OBJS_DIR), $(OBJS))
 
-$(NAME): $(OBJS)
-	ar rcs $@ $^
+###
 
-all: $(NAME)
+all:			$(NAME)
+
+$(OBJS_DIR)%.o: $(SRCS_DIR)%.c
+				@mkdir -p $(OBJS_DIR)
+				@echo "$(YELLOW)Compiling: $< $(DEF_COLOR)"
+				@$(CC) $(CFLAGS) -I $(INCLUDES) -c $< -o $@
+
+$(NAME):		$(OBJS_PREFIX) $(OBJS_DIR) $(INCLUDES)
+				@make -C $(LIBFT)
+				@cp libft/libft.a .
+				@mv libft.a $(NAME)
+				@$(AR) $(NAME) $(OBJS_PREFIX)
+				@echo "$(GREEN)ft_printf compiled!$(DEF_COLOR)"
 
 clean:
-	$(RM) $(OBJS) $(OBJS_B)
+				@$(RM) -r $(OBJS_DIR)
+				@make clean -C $(LIBFT)
+				@echo "$(BLUE)ft_print binary files cleaned!$(DEF_COLOR)"
 
-fclean: clean
-	$(RM) $(NAME)
+fclean:			clean
+				@$(RM) $(NAME)
+				@$(RM) $(LIBFT)/libft.a
+				@echo "$(CYAN)ft_printf executable files cleaned!$(DEF_COLOR)"
+				@echo "$(CYAN)libft executable files cleaned!$(DEF_COLOR)"
 
-re: clean all
+re:				fclean all
+				@echo "$(GREEN)Cleaned and rebuilt everything for ft_printf!$(DEF_COLOR)"
+
+.PHONY:			all clean fclean re
--- includes/ft_printf.h
+++ includes/ft_printf.h
@@ -1,50 +1,26 @@
-/* ************************************************************************** */
-/*                                                                            */
-/*                                                        :::      ::::::::   */
-/*   ft_printf.h                                        :+:      :+:    :+:   */
-/*                                                    +:+ +:+         +:+     */
-/*   By: ftudini <ftudini@student.42.fr>            +#+  +:+       +#+        */
-/*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2021/03/24 15:47:49 by ftudini           #+#    #+#             */
-/*   Updated: 2021/10/06 18:56:12 by ftudini          ###   ########.fr       */
-/*                                                                            */
-/* ************************************************************************** */
-
 #ifndef FT_PRINTF_H
-#define FT_PRINTF_H
+# define FT_PRINTF_H
 
-#include <unistd.h>
-#include <stdlib.h>
-#include <string.h>
-#include <stdarg.h>
-#include <limits.h>
-#include <wchar.h>
-#include <stdio.h>
+# include <stdarg.h>
+# include <unistd.h>
+# include "../includes/libft.h"
 
-typedef struct s_flags
-{
-	size_t i;
-	size_t ret;
-} t_flags;
+int		ft_printf(const char *format, ...);
+int		ft_parsing(va_list args, const char format);
+int		ft_printchar(int c);
+int		ft_printstr(char *str);
+int		ft_print_ptr(unsigned long long ptr);
+int		ft_printnbr(int n);
+int		ft_print_unsigned(unsigned int n);
+int		ft_print_hex(unsigned int num, const char format);
+int		ft_printpercent(void);
 
-int ft_printf(const char *format, ...);
-int check_char(char c);
-void print_normal(t_flags *flags, char *format);
-void init_flags(t_flags *flags);
-void ft_handle_char(char c, t_flags *flags);
-void ft_parsing(t_flags *flags, char *format, va_list ap);
-void ft_putstr_fd(char *s, int fd);
-void ft_putnbr_fd(int n, int fd);
-int ft_strlen(const char *str);
-void ft_putchar_fd(char c, int fd);
-int count_digits(int n);
-char *ft_itoa(int n);
-void ft_putnbr_base(unsigned long int nbr, char *base);
-unsigned int ft_lenbase(char *base);
-int count_digits_hex(unsigned long int n);
-int count_digits_short(unsigned int n);
-void ft_handle_hexa_low(unsigned int n, t_flags *flags);
-void ft_handle_hexa_up(unsigned int n, t_flags *flags);
-void ft_handle_unsi(unsigned int unsi, t_flags *flags);
+void	ft_putstr(char *str);
+void	ft_put_ptr(unsigned long long num);
+int		ft_ptr_len(unsigned long long num);
+char	*ft_uitoa(unsigned int n);
+int		ft_num_len(unsigned	int num);
+void	ft_put_hex(unsigned int num, const char format);
+int		ft_hex_len(unsigned	int num);
 
 #endif
